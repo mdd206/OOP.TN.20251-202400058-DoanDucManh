840707b3f4a10f4ca08c8615f7538a6a6cfd02a2
mdd206 <mdd206@gmail.com>
2025-12-19 16:26:45 +0700

    painter javafx

--- AimsProject/src/module-info.java
+++ AimsProject/src/module-info.java
@@ -5,4 +5,12 @@
  * 
  */
 module AimsProject {
+	requires transitive javafx.controls;
+    requires transitive javafx.fxml;
+    requires transitive javafx.graphics;
+    requires transitive javafx.base;
+    
+    opens com.hust.kstn.javafx to javafx.fxml;
+    
+    exports com.hust.kstn.javafx;
 }
